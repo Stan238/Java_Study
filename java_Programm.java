9b4ddfbeacdc1d9f3acfffdf32156ec886996d08
Stan238 <28St03R@protonmail.com>
2026-02-23 11:35:17 -0500

    Refactor java_Programm to replace car details and currency conversion with string manipulation examples

--- java_Programm.java
+++ java_Programm.java
@@ -3,26 +3,20 @@ public class java_Programm {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
 
-        String make = "Ford";
-        String model = "Mustang";
-        int year = 2024;
-        double mileage = 15002.5;
-        Boolean isAutomatic = true;
-        
-        System.out.println("Car Make: " + make);
-        System.out.println("Car Model: " + model);
-        System.out.println("Car Year: " + year);
-        System.out.println("Car Mileage: " + mileage + " miles");
-        System.out.println("Is the car automatic: " + isAutomatic);
-         double amountInUSD = 100.0; 
-double exchangeRateUSDToEUR = 0.85; 
-System.out.println("Initial amount: "+ amountInUSD +" USD" );
-double amountEUR = amountInUSD * exchangeRateUSDToEUR;
-System.out.println("the amount in EUR after conversion " + amountEUR +" EUR" );
-amountEUR -= 55;
-System.out.println("the amount in EUR after deduct " + amountEUR +" EUR" );
-amountInUSD = amountEUR / exchangeRateUSDToEUR;
- System.out.printf("Amount in USD after converting back: $%.2f", amountInUSD);
+       String sentence = "Java programming is fun and educational" ;
+       String searchWord = "fun";
+       boolean containsWord = sentence.contains(searchWord);
+         System.out.println("Does the sentence contain the word '" + searchWord + "'? " + containsWord);
+       String newSentence = sentence.replace("educational", "awesome");
+         System.out.println("Modified sentence: " + newSentence);
+         System.out.println("Length of the original sentence: " + sentence.length());
+        System.out.println((" Cut sentece 17 characters: " + sentence.substring(0,17)));
+
+        System.out.println(sentence.toUpperCase());
+         System.out.println(sentence.toLowerCase());
+         System.out.println(searchWord.equals("fun"));
+         
+      
     }
 
 }
